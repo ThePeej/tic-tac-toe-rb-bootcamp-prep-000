32aac0afd61b57c56101150313c0fe540c82eab1
PJ Frias <peejfrias@gmail.com>
2017-11-22 12:13:44 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,6 +11,75 @@ WIN_COMBINATIONS = [
 
 #helper methods
 
+#display_board creates a blank tic tac toe board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts "-----------"
+end
+
+#input_to_index converts a user_input to an integer
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+#move allows "X" player in the bottom right and "O" in the top left
+def move(board, position, player = "X")
+  board[position] = "#{player}"
+end
+
+#valid_move? returns true/false based on index
+def valid_move?(board, index)
+  if index.between?(0,8)
+    position_taken?(board,index)
+  else
+    false
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    false
+  else
+    true
+  end
+end
+
+#turn puts everything all together
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board,index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  count = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      count += 1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  count = turn_count(board)
+  if count % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
